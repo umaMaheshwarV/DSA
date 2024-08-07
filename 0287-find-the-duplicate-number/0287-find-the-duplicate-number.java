79b1312e9f0a05dcfa642c3eb37d62c07ac25da8
umaMaheshwarV <umamaheshwar.vulavala@gmail.com>
2024-08-07 22:42:53 +0530

    Time: 1 ms (100%), Space: 58.5 MB (20.22%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,15 +1,16 @@
+//Intial approach -> iterate the array thru two loops and if two elements are equal then returnt the value
+//optimised --
+//->To intialize a boolean array of length.
+//-->Mark all the array elements to true, so that for a repetetive element it returns the duplicate element.
 class Solution {
     public int findDuplicate(int[] nums) {
         int len=nums.length;
         int dup=0;
         boolean[] find = new boolean[len];
-        for(int i=0;i<len;i++){
-            if(!find[nums[i]]){
-                find[nums[i]] = true;
-            }else{
-                dup = nums[i];
-            }
+        for(int i : nums){
+            if(find[i]) return i; 
+            find[i] = true;
         }
-        return dup;
+        return -1;
     }
 }
