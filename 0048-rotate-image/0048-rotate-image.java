26ee1c69ea8f4b5ee333a3920988492d8875969b
umaMaheshwarV <umamaheshwar.vulavala@gmail.com>
2024-08-10 18:31:35 +0530

    Time: 0 ms (100%), Space: 42.3 MB (13.82%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,7 +1,10 @@
+// inter change the top and bottom row
+// swap the columns
 class Solution {
     public void rotate(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
+        //Interchange the top and bottom row
         for(int i=0;i<row/2;i++){
             for(int j=0;j<col;j++){
                 int temp=matrix[i][j];
@@ -9,6 +12,7 @@ class Solution {
                 matrix[row-1-i][j]=temp;
             }
         }
+        //swap the diagonal columns
         for(int i=0;i<row;i++){
             for(int j=i;j<col;j++){
                 int temp=matrix[i][j];
