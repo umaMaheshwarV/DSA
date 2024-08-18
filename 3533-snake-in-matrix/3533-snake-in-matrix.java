336cce85831b6f6736de0fd047f5dd7a6851d7e1
umaMaheshwarV <umamaheshwar.vulavala@gmail.com>
2024-08-19 01:22:13 +0530

    Time: 2 ms (97.02%), Space: 44 MB (92.92%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        int upd= 0;
+        for(String inst: commands){
+            
+            if(inst.equals("DOWN")){
+                upd+=n;
+            } 
+            if(inst.equals("UP")) upd-=n;
+            if(inst.equals("LEFT")) upd-=1;
+            if(inst.equals("RIGHT")) upd+=1;
+            //System.out.println("cojunt"+inst+"ssv"+upd);
+        }
+        return upd;   
+    }
+}
