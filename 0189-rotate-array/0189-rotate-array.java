7b64cb6e3db4863b46bfa1dcfcd1e984bc3e6327
umaMaheshwarV <umamaheshwar.vulavala@gmail.com>
2024-10-03 23:41:55 +0530

    Time: 0 ms (100%), Space: 57.6 MB (23.15%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,15 +1,13 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int len=nums.length;
-        if(k>=len) {
             k%=len;
-        }
         arrrev(len-k,len-1,nums);
         arrrev(0,len-k-1,nums);
         arrrev(0,len-1,nums);
     }
     public void arrrev(int first,int last,int[] nums){
-        System.out.println("first"+first+"last"+last);
+        //System.out.println("first"+first+"last"+last);
         while(first<last){
             int temp = nums[first];
             nums[first]=nums[last];
